c0f4562147ef6c1bd3bb303151b7147b15849282
vagnero <vagnerimperador16@gmail.com>
2024-05-28 15:30:40 -0300

    Entidade user atualizada

--- src/user/user.entity/user.entity.ts
+++ src/user/user.entity/user.entity.ts
@@ -1,1 +1,20 @@
-export class User {}
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
+import { Movie } from 'src/movie/movie.entity/movie.entity';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  nome: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  senha: string;
+
+  @OneToMany(() => Movie, movie => movie.user)
+  movies: Movie[];
+}
